d37f605a651d52a88491ea8a1d61f54c8d3596c1
FoziaAkhtar <fozia.shah0001@gmail.com>
2026-07-15 11:29:42 -0400

    Initial Commit

---
+++ iOSApp6/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSApp6
+//
+//  Created by Fozia on 2026-07-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSApp6/iOSApp6App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp6App.swift
+//  iOSApp6
+//
+//  Created by Fozia on 2026-07-15.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
